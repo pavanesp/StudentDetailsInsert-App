fb0c2f89d8c1963df582d3f602472c9ddeef3eeb
pavanesp <pavankumarppk29@gmail.com>
2019-06-17 10:04:33 -0800

    code changes sysout

--- src/main/java/com/nt/controller/WelcomeSimpleFormController.java
+++ src/main/java/com/nt/controller/WelcomeSimpleFormController.java
@@ -36,7 +36,7 @@ public class WelcomeSimpleFormController extends SimpleFormController{
 		StudentCommand cmd=null;
 		cmd=(StudentCommand)command;
 		StudentDto dto=null;
-		
+		System.out.println("hello");
 		dto=new StudentDto();
 		
 		BeanUtils.copyProperties(cmd, dto);
